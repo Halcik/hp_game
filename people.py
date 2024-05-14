0cf93b4adf8814573c4328b60136539f5a4028a6
Halcik <julkatablet2507@gmail.com>
2024-05-14 21:39:04 +0200

    Test for new_person in class Person, new class Family

--- people.py
+++ people.py
@@ -1,22 +1,38 @@
-import pyinputplus as pyip
 
-class Person:
+class Family:
+  def __init__(self, person, dad=None, mom=None, love=None):
+    self.me = person
+    self.father = dad
+    self.mother = mom
+    self.love = love
 
-  def __init__(self, first_name=None, last_name=None, age=0, mother=None, father=None):
+  def __del__(self):
+    del self.me
+    del self.father
+    del self.mother
+    del self.love
+
+  def set_parents(self, mom, dad):
+    self.mother = mom
+    self.father = dad
+
+  def love(self, my_love):
+    self.love = my_love
+
+class Person(Family):
+
+  def __init__(self, first_name=None, last_name=None, age=0):
+    super().__init__(self)
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
-    self.mother = mother
-    self.father = father
-    self.love = None
+    self.personality = []
 
   def __del__(self):
     del self.first_name
     del self.last_name
     del self.age
-    del self.mother
-    del self.father
-    del self.love
+
 
   def __str__(self):
     information = f"\n~~~ Info about {self.first_name} {self.last_name} ~~~:\nAge: {self.age}"
@@ -38,21 +54,17 @@ class Person:
   def give_me_name(self, f_name, l_name):
     self.first_name = f_name
     self.last_name = l_name
-
-  def set_parents(self, mom, dad):
-    self.mother = mom
-    self.father = dad
-
-  def i_love(self, my_love):
-    self.love = my_love
   
   @classmethod
   def new_person(cls):
-    name = pyip.inputStr(prompt="First name: ", blockRegexes=[r"[0-9]"]).title()
-    l_name = pyip.inputStr(prompt="Last name: ", blockRegexes=[r"[0-9]"]).title()
-    age = pyip.inputInt(prompt="Age: ", min=0, max=200)
+    name = input("First name: ").title()
+    l_name = input("Last name: ").title()
+    age = int(input("Age: "))
+    if not name or not l_name:
+      return -1
     return cls(name, l_name, age)
   
-p1 = Person().new_person()
-print(p1)
+if __name__ == '__main__':
+  p1 = Person().new_person()
+  print(p1)
 
--- test_person.py
+++ test_person.py
@@ -1,6 +1,8 @@
 import people
+from unittest import mock
+from unittest import TestCase
 
-def test_create__first_ready_person(): #wihout family, first
+def test_first_ready_person(): #wihout family, first
   p1 = people.Person("Anna", "Hill", 36)
   assert p1.first_name=="Anna"
   assert p1.last_name=="Hill"
@@ -8,7 +10,7 @@ def test_create__first_ready_person(): #wihout family, first
   assert p1.mother==None
   assert p1.father==None
 
-def test_create_first_unready_person(): #wihout family, first with
+def test_first_unready_person(): #wihout family, first with
   p1 = people.Person()
   assert p1.first_name==None
   assert p1.last_name==None
@@ -16,6 +18,35 @@ def test_create_first_unready_person(): #wihout family, first with
   assert p1.mother==None
   assert p1.father==None
 
-def test_birth_new_person():
-  pass
+class Test_new_person(TestCase):
+  @mock.patch('people.input', create=True)
+  def test_good_person(self, mocked_input):
+    mocked_input.side_effect = ['july', 'rose', '5']
+    result = people.Person().new_person()
+    assert result.first_name=='July'
+    assert result.last_name=='Rose'
+    assert result.age == 5
 
+  @mock.patch('people.input', create=True)
+  def test_no_name(self, mocked_input):
+    mocked_input.side_effect = ['', '', 0]
+    result = people.Person().new_person()
+    assert result==-1
+
+  @mock.patch('people.input', create=True)
+  def test_number_in_name(self, mocked_input):
+    mocked_input.side_effect = ['5', '4', 5]
+    result = people.Person().new_person()
+    assert result.first_name.isalpha()==True
+
+  @mock.patch('people.input', create=True)
+  def test_str_in_age(self, mocked_input):
+    mocked_input.side_effect = ['Aka', 'Deka', 'fefs']
+    result = people.Person().new_person()
+    assert result==-1
+
+  @mock.patch('people.input', create=True)
+  def test_negative_age(self, mocked_input):
+    mocked_input.side_effect = ['Aka', 'Deka', -10]
+    result = people.Person().new_person()
+    assert (result.age>0)==True
